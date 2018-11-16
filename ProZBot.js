5bcdd3449da867f754346e9aaa6207707dbd10c6
SenkiDaKitty <44231818+SenkiDaKitty@users.noreply.github.com>
2018-11-16 18:45:38 +0100

    Update ProZBot.js

--- ProZBot.js
+++ ProZBot.js
@@ -22,8 +22,9 @@ client.on('message', message => {
 if (message.content === "Plox i want sum turtle twerk") {
 	message.channel.sendMessage("https://cdn.discordapp.com/attachments/506530289053466625/507636544400654363/tenor.gif")
 	return
-        }
+}
 });
+}
 client.on('message', async message => {
 	    console.log(`Content : ${  message.content}`);
 if (message.content === prefix + "list") {
@@ -427,7 +428,6 @@ var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
              if (!serverQueue) return message.channel.send("[ProzBot - MusicSystem] - There are currently no music playing.")
             if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
     }
-}
 });
 
 client.login(process.env.BOT_TOKEN);
