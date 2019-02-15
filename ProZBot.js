f839d74ecebec0bd650553f06ba3a6f1ecbab5ce
SenkiDaKitty <44231818+SenkiDaKitty@users.noreply.github.com>
2019-02-15 01:01:13 +0100

    Update ProZBot.js

--- ProZBot.js
+++ ProZBot.js
@@ -552,7 +552,8 @@ client.on('message', message => {
          message.channel.send(demande);
          var demandeS = new Discord.RichEmbed()
                  .setAuthor(message.author.username, message.author.avatarURL)
-                 .setDescription(`Hello, you received a report of ${message.author.tag}. The Report :`,`${messageaides}`)
+                 .addField(`Hello, you received a report of ${message.author.tag}. The Report :`)
+                 .addField(`${messageaides}`)
                  .setColor("0x02e427")
                  .setFooter("ProzBot, Any hacking attempts will lead to a lawsuit ⚠.","https://cdn.discordapp.com/attachments/434459534514454528/504356020198572044/MIUI-9-Gif-Lightning.gif?width=473&height=473")
                  const reportlogs = message.guild.channels.find(channel => channel.name === "reportlogs");
